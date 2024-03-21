034c3c3222ba9b0ced939beff6c4f964553ea113
FZQKhaos <casjen01@easv365.dk>
2024-03-21 14:30:19 +0100

    DB Connector, Event BE

---
+++ src/sample/BE/Event.java
@@ -0,0 +1,100 @@
+package sample.BE;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+
+public class Event {
+
+    private int id;
+    private String name;
+
+    //private List<Eventkoordinator> eventkoordinator;
+    private int tickets;
+    private String location;
+    private Date startDate;
+    private Date endDate;
+    private String startTime;
+    private String endTime;
+    private String description;
+
+    public Event(int id, String name, int tickets, String location, Date startDate, Date endDate, String startTime, String endTime, String description) {
+        this.id = id;
+        this.name = name;
+        this.tickets = tickets;
+        this.location = location;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(int tickets) {
+        this.tickets = tickets;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
