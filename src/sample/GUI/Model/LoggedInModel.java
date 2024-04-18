5d5e2153515f79724198bbe6945d7505a2e1d506
NewPhase14 <jepbad01@easv365.dk>
2024-04-18 21:56:08 +0200

    Updated for what we use

--- src/sample/GUI/Model/LoggedInModel.java
+++ src/sample/GUI/Model/LoggedInModel.java
@@ -5,32 +5,14 @@ import sample.BE.EventCoordinator;
 
 public class LoggedInModel {
     private static LoggedInModel instance;
-    private boolean loggedIn;
     private Admin admin;
     private EventCoordinator eventCoordinator;
 
-    // Private constructor to prevent instantiation from outside
-    private LoggedInModel() {
-        loggedIn = false;
-    }
-
-    // Method to get the instance of LoggedInModel
     public static LoggedInModel getInstance() {
         if (instance == null) {
             instance = new LoggedInModel();
-
         }
         return instance;
-    }
-
-    // Method to set the login status
-    public void setLoggedIn(boolean status) {
-        loggedIn = status;
-    }
-
-    // Method to get the login status
-    public boolean isLoggedIn() {
-        return loggedIn;
     }
 
     public void setUser(Admin admin, EventCoordinator eventCoordinator){
